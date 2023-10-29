f0ccea5c73dbf47cbe7069e53551e2264fc7ce92
smplcod <mplcod2021@gmail.com>
2023-10-30 02:33:33 +0600

    json output form add

--- src/components/InfTable.jsx
+++ src/components/InfTable.jsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./InfTable.module.css"; // Импортируем стили
 import StatusBar from "./StatusBar";
 
 const InfTable = ({ rows = 5, cols = 2 }) => {
+  const [tableData, setTableData] = useState({}); // Для хранения данных таблицы
   const colNames = []; // A to ZZ
+
   for (let i = 65; i <= 90; i++) {
     colNames.push(String.fromCharCode(i));
   }
+
   for (let i = 65; i <= 90; i++) {
     for (let j = 65; j <= 90; j++) {
       colNames.push(String.fromCharCode(i) + String.fromCharCode(j));
@@ -14,6 +17,17 @@ const InfTable = ({ rows = 5, cols = 2 }) => {
   }
 
   const limitedColNames = colNames.slice(0, cols);
+
+  const handleSave = () => {
+    localStorage.setItem("tableData", JSON.stringify(tableData));
+  };
+
+  const handleLoad = () => {
+    const loadedData = localStorage.getItem("tableData");
+    if (loadedData) {
+      setTableData(JSON.parse(loadedData));
+    }
+  };
 
   return (
     <>
@@ -32,6 +46,19 @@ const InfTable = ({ rows = 5, cols = 2 }) => {
             </div>
           ))
         )}
+      </div>
+      <div className={styles.controlPanel}>
+        <div className={styles.buttons}>
+          <button onClick={handleSave}>Save</button>
+          <button onClick={handleLoad}>Load</button>
+        </div>
+        <textarea
+          className={styles.textarea}
+          rows="4"
+          cols="50"
+          readOnly
+          value={JSON.stringify(tableData, null, 2)}
+        ></textarea>
       </div>
       <StatusBar
         cmdctrl={true}
