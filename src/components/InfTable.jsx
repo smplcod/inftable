e352ba9c584735ed2535f238a060fbb08467dc9c
smplcod <mplcod2021@gmail.com>
2023-10-29 22:24:59 +0600

    MVP table v1.3

--- src/components/InfTable.jsx
+++ src/components/InfTable.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 
-const InfTable = ({ rows = 100, cols = 50 }) => {
+const InfTable = ({ rows = 30, cols = 4 }) => {
+  // Рассчитываем минимальную ширину для внутреннего div
+  const minWidth = 300 * cols; // 300px - ширина каждой ячейки
+
   return (
-    <div style={{ overflow: "auto", width: "100%", height: "100vh" }}>
-      <div style={{ display: "flex", flexDirection: "column" }}>
+    <div
+      style={{
+        overflow: "auto",
+        width: "50%",
+        height: "50%",
+        position: "relative",
+      }}
+    >
+      <div style={{ minWidth: `${minWidth}px`, position: "absolute" }}>
         {Array.from({ length: rows }, (_, rowIndex) => (
           <div
             key={rowIndex}
             style={{
-              whiteSpace: "nowrap", // оставляем только это свойство
+              whiteSpace: "nowrap",
             }}
           >
             {Array.from({ length: cols }, (_, colIndex) => (
@@ -18,19 +28,17 @@ const InfTable = ({ rows = 100, cols = 50 }) => {
                   border: "1px solid black",
                   width: "300px",
                   height: "50px",
-                  display: "inline-block", // изменяем на inline-block
-                  alignItems: "center",
-                  justifyContent: "center",
+                  display: "inline-block",
                 }}
               >
-                <input
+                {/* <input
                   type="text"
                   style={{
                     width: "100%",
                     height: "100%",
                     border: "1px solid grey",
                   }}
-                />
+                /> */}
               </div>
             ))}
           </div>
